2375fbbef2dbac1b1be0a3019a2549c0433c07dd
Jesse Wenger <wengerj@gmail.com>
2021-05-10 17:26:00 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :products, only: [:index]
 
-  get "/", to: 'products#homepage'
+  get "/", to: 'products#index'
   post "/", to: 'products#add'
 end
