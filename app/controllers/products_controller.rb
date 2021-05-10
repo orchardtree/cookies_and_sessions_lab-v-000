405cd11afd332a278da67f43d2dfe241b48178a8
Jesse Wenger <wengerj@gmail.com>
2021-05-10 16:27:02 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,9 +6,4 @@ class ProductsController < ApplicationController
     @product = params[:product]
     cart << @product
   end
-
-  #private
-  #def product_params
-  #  params.require(:product).permit(:product)
-  #end
 end
